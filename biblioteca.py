1f2b53eded394af72b071527dc287a89a239bbe4
Guilherme Lima <enriqueguilherme1209@gmail.com>
2025-05-06 11:30:19 -0300

    Corrigir em casa uma acao so pode seguir se a outra nao estiver em acao

--- biblioteca.py
+++ biblioteca.py
@@ -3,9 +3,21 @@ class Pessoa():
         self.nome = nome
         self.idade = idade
         self.peso = peso
+        self.falando = False
+        self.comendo = False
+        self.dormindo = False
+
+
     def falar(self):
-        print(f"{self.nome} começou a falar")
-    def comer(self):
-        print(f"{self.nome} está comendo")
+            if self.falando == False:
+                comendo = False
+                dormindo = False
+            print(f"{self.nome} começou a falar")
+
+    def comer(self, comendo):
+        print(f"{self.nome} está comendo: {comendo}")
+        if self.comendo == False:
+
+
     def dormir(self):
         print(f"{self.nome} dormiuZZZzzzZZZzzz")
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from biblioteca import Pessoa
 
-aluno01 = Pessoa("Guilherme", 21, 82)
-aluno01 = Pessoa("Victor", 20, 76)
+#print(aluno01.nome, aluno01.peso,aluno01.idade)
 
-print(aluno01.nome, aluno01.peso,aluno01.idade)
+aluno01 = Pessoa("Guilherme", 21, 82)
+aluno02 = Pessoa("Victor", 20, 76)
+
+aluno01.falar()
+aluno01.comer()
